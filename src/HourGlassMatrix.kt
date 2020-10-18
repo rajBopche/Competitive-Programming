e782b81af2b57343673a9d9812a8bfadec942c16
rajbopche <raj.bopche@paynearby.in>
2020-10-18 23:00:09 +0530

    prob_4 | Solved HourGlassMatrix problem - Hackerrank

---
+++ src/HourGlassMatrix.kt
@@ -0,0 +1,17 @@
+class HourGlassMatrix {
+
+    fun hourglassSum(arr: Array<Array<Int>>): Int {
+        val sumOfAllHourGlasses = ArrayList<Int>(16)
+
+        for (i in 0 until (arr.size - 2))
+            for (j in 0 until (arr.size - 2)) {
+
+                val sumOfHourGlass =
+                    arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+
+                sumOfAllHourGlasses.add(sumOfHourGlass)
+            }
+        return sumOfAllHourGlasses.max()!!
+    }
+
+}
