525a01cf55301a145d95191e3a2e0d5b670eff2c
rajbopche <raj.bopche@paynearby.in>
2020-10-21 06:23:00 +0530

    prob_5 | Solve Array Left Rotation - Hackerrank

---
+++ src/ArrayLeftRotation.kt
@@ -0,0 +1,20 @@
+class ArrayLeftRotation {
+
+    fun rotLeft(a: Array<Int>, d: Int): Array<Int> {
+
+        val newTempArray = a.clone()
+        val arraySize = a.size
+        if (d == arraySize) return a
+        for (i in a.indices) {
+
+            var newPos = i - d
+            if (newPos < 0) newPos += arraySize
+
+            a[newPos] = newTempArray[i]
+
+
+        }
+        return a
+    }
+
+}
