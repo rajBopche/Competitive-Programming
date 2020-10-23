076665b5e9bcf1a4823e65d08ef0653dcc0ad2f8
rajbopche <raj.bopche@paynearby.in>
2020-10-23 10:03:07 +0530

    prob_6 | Complete NewYearChaos probelm - Hackerrank

---
+++ src/NewYearChaos.kt
@@ -0,0 +1,35 @@
+class NewYearChaos {
+
+    fun minimumBribes(q: Array<Int>) {
+
+        var minBribes = 0
+
+        if (q.size == 1) {
+            println(0)
+            return
+        }
+
+        for (i in (q.size - 1) downTo 0) {
+            if (q[i] != (i + 1)) {
+
+                when {
+                    q[i - 1] == (i + 1) -> {
+                        ++minBribes
+                        q[i - 1] = q[i].also { q[i] = q[i - 1] }
+                    }
+                    q[i - 2] == (i + 1) -> {
+                        minBribes += 2
+                        q[i - 2] = q[i - 1]
+                        q[i - 1] = q[i]
+                        q[i] = i + 1
+                    }
+                    else -> {
+                        println("Too chaotic")
+                        return
+                    }
+                }
+            }
+        }
+        println(minBribes)
+    }
+}
